15f7090805ca94257026e74825959e511e2c1383
Wojtini <whitem200@gmail.com>
2022-10-30 17:31:55 +0100

    fix master

--- app/start_prod.sh
+++ app/start_prod.sh
@@ -4,6 +4,11 @@ cd app || exit
 rm /app/staticfiles -r
 python manage.py collectstatic
 python manage.py compress --force
+
+python manage.py crontab add
+python manage.py crontab show
+service cron start
+
 gunicorn --bind 0.0.0.0:8000 --workers=6 Masquerade.wsgi &
 P1=$!
 daphne -b 0.0.0.0 -p 8010 Masquerade.asgi:application &
